5b021ca0a4c6e46487196c81abccdb932cdfda11
Jeremias Freitas <jeremias.freitas@dtidigital.com.br>
2025-07-04 15:29:18 -0300

    first commit

---
+++ src/main/java/com/jfreitas/challange_restaurant_marketplace/ChallangeRestaurantMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.jfreitas.challange_restaurant_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeRestaurantMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangeRestaurantMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfreitas/challange_restaurant_marketplace/ChallangeRestaurantMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfreitas.challange_restaurant_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangeRestaurantMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
